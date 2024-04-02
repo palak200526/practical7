f0dd824310e8661d856a867aeeba033bd9c94877
palak <palakgrover2611@gmail.com>
2024-04-02 11:03:13 +0530

    practical7

---
+++ practical7_1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int age;
+    printf("enter the age:");
+    scanf("%d",&age);
+    printf("age of a person is %d\n", age);
+    return 0;
+}
